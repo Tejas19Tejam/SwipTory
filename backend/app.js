e6fab21bff75e62ad1a4e3d815193e5f2f94c61c
Vasudev Tejam <88923558+Tejas19Tejam@users.noreply.github.com>
2024-05-02 22:25:01 +0530

    Cmpress package install

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const compression = require("compression");
 const mongoSanitize = require("express-mongo-sanitize");
 const globalErrorHandler = require("./src/controller/errorController");
 const AppError = require("./src/utils/appError");
@@ -25,6 +26,8 @@ app.use(
   })
 );
 
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // Data sanitization against NoSQL query injection
@@ -33,16 +36,10 @@ app.use(mongoSanitize());
 // Body parser , reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 
-// Development logging
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
-
 app.use(morgan("dev"));
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/stories", storyRoutes);
-// app.use("/api/v1/bookmarks", storyRoutes);
 
 // This middleware will run for all the HTTP request (ie GET , POST  , PATCH etc )
 app.all("*", (req, res, next) => {
